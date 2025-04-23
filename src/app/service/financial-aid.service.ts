154ece45823e26258af5040c533752c08c5facf2
ChihebDabebi <dabebi.chiheb@esprit.tn>
2025-04-23 16:15:10 +0100

    fixx

--- src/app/service/financial-aid.service.ts
+++ src/app/service/financial-aid.service.ts
@@ -16,7 +16,7 @@ export interface FinancialAidRequest {
   providedIn: 'root'
 })
 export class FinancialAidService {
-  baseUrl = 'http://localhost:8090/finance';
+  baseUrl = 'http://localhost:3000/finance';
 
   constructor(private http: HttpClient) {}
 
